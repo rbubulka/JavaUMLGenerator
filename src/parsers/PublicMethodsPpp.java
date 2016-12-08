cd4c95ae0dbda1c27f76485226857531b38a6aab
SongyuWang <wangsongye@126.com>
2016-12-07 22:59:20 -0500

    end of the night, just started to right ppps

--- src/parsers/PublicMethodsPpp.java
+++ src/parsers/PublicMethodsPpp.java
@@ -2,10 +2,15 @@ package parsers;
 
 import java.util.List;
 
+import org.objectweb.asm.tree.MethodNode;
+
 public class PublicMethodsPpp extends MethodsPpp {
 	@Override
 	public String parse(List methods) {
-		// TODO Auto-generated method stub
+		for(MethodNode md:(List<MethodNode>)methods){
+			String result="";
+//			result+=md.
+		}
 		return null;
 	}
 }
