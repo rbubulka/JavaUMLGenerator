cb3b1f2678bb428b0b0df6e8bd8b5c0c9fc1b925
Songyu Wang <wangs4@rose-hulman.edu>
2017-01-17 21:15:37 -0500

    try to refator sig

--- src/utilities/SignatureInterpreter.java
+++ src/utilities/SignatureInterpreter.java
@@ -1,5 +1,46 @@
 package utilities;
 
-public class SignatureInterpreter {
+import java.util.Arrays;
+import java.util.List;
 
+public class SignatureInterpreter {
+	public void interpret(String s, List<String> res){
+
+	if(!s.contains("<")){
+		List<String> strs= Arrays.asList(s.split(";"));
+		res.addAll(strs);
+	}
+	else{
+		int count=0;
+		String temp="";
+		for(int i=0;i<s.length();i++){
+			char c=s.charAt(i);
+			switch (c) {
+	         case '<':
+	            count++;
+	            String container=temp;
+	            temp="";
+	             break;
+	         case '>':
+	        	 count--;
+	        	 interpret(temp, res);
+	        	 temp="";
+	        	 break;
+	         case ';':
+	        	 if (count%2==0){
+	        		 interpret(temp, res);
+	        		 temp="";
+	        	 }else{
+	        	 temp+=c;
+	        	 }
+	        	 break;
+	         default:
+	        	 temp+=c;
+	        	 break;
+	             
+	     }
+		}
+	}
+		
+	}
 }
