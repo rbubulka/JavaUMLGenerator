20a3bbff9191868dec900f4c822e40121b28bd67
bubulkr <bubulkr@rose-hulman.edu>
2017-01-27 14:58:16 -0500

    cleaning

--- src/utilities/AvoidNonclassUtils.java
+++ src/utilities/AvoidNonclassUtils.java
@@ -5,7 +5,7 @@ import java.util.List;
 
 public class AvoidNonclassUtils {
 	
-	private static String[] nonclass = {"B","C","D","F","I","J","S","Z","V",">","*", "JI",")V",")I",")Z"};
+	private static String[] nonclass = {"B","C","D","F","I","J","S","Z","V",">","*", "JI",")V",")I",")Z","IIIII",")F","ZZ","JJ","BB","SS","CC","FF","DD","TT",")J","ZJ","IZ",")TT"};
 	private static List<String> classes = Arrays.asList(nonclass);
 	public static boolean isAClass(String signature){
 		if(signature == null||signature.equals("")) return false;
