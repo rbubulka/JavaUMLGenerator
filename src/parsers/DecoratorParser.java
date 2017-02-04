73e5ed78435af7dcdcd35a6acaa4a68bebde932d
Songyu Wang <wangs4@rose-hulman.edu>
2017-02-04 18:28:04 -0500

    hopefully done with decrotor

--- src/parsers/DecoratorParser.java
+++ src/parsers/DecoratorParser.java
@@ -1,11 +1,19 @@
 package parsers;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
+import javax.swing.plaf.synth.SynthSeparatorUI;
+
+import org.objectweb.asm.ClassReader;
 import org.objectweb.asm.Opcodes;
 import org.objectweb.asm.tree.ClassNode;
+import org.objectweb.asm.tree.MethodNode;
 
 import parsers.ClassParser.ClassParser;
 
@@ -25,10 +33,80 @@ public class DecoratorParser extends ClassParser {
 			//if cn.fields contains interface.type then mark true selffield
 			//if takesself&&selffield then component 
 			//if takessuper&&selffield then decorator
+			ClassNode current=cn;
+			List<MethodNode> thismls=(List<MethodNode>)current.methods;
+		Set<String> paths=new HashSet<String>();
+		Set<String> comp=new HashSet<String>();
+					boolean xxx=helper(cn, paths,comp);
+					if(xxx){
+						for(String x:paths){
+							System.out.println("final p " +x);
+						}
+						for(String x:comp){
+							System.out.println("final c " +x);
+						}
+					}
+					System.out.println(xxx);
+					
+			
+			
+			
 			
 			
 		}
 		if(otherparser !=  null)result.append(this.otherparser.parse(nodes, relations, classinfo));
 		return result.toString();
+	}
+
+	private boolean helper(ClassNode cn,Set<String>path,Set<String> comp) {
+		List<MethodNode> methodls=cn.methods;
+		Set<String> typels=new HashSet<String>();
+		//get all the classes passed as arguments in the constructor
+		for(MethodNode md:methodls){
+			if(md.name.contains("<init>")){
+				String desc=md.desc;
+				desc.substring(desc.indexOf("(")+1, desc.indexOf(")"));
+				if(!desc.equals("")){
+					typels.addAll(new ArrayList(Arrays.asList(desc.split(";"))));
+					typels.remove("");
+				}
+				
+			}
+		}
+		ClassNode currentNode = cn;
+		while(true){
+			//check if not at object
+		String supername=currentNode.superName;
+		if(supername==null){
+			return false;
+		}
+		//get all the interfaces of the current parent to theoriginal class
+		Set<String> interfacels=new HashSet<String>();
+		for(String temp:(List<String>)currentNode.interfaces){
+			
+			interfacels.add(temp);
+		}
+		interfacels.add(currentNode.name);
+		//check if the original class takes one of the interfaces as an arguemtn
+		for(String interfacename:interfacels){
+			for(String tp:typels){
+				if(tp.contains(interfacename)||interfacename.contains(tp)){ //if it does then is a decorator
+					comp.add(interfacename);
+					return true;
+				}
+			}
+		}
+		//if not go to super
+		ClassReader cr=null;
+		try {
+			cr = new ClassReader(supername);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		ClassNode next = new ClassNode();
+		cr.accept(next, ClassReader.EXPAND_FRAMES);
+		path.add(cn.name);
+		currentNode = next;
+		}
 	}
 }
