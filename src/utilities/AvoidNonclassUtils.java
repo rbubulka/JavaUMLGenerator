a5b1e64a0df7fe78264ee01967e09742a84e8851
bubulkr <bubulkr@rose-hulman.edu>
2017-02-10 00:07:41 -0500

    cleaning

--- src/utilities/AvoidNonclassUtils.java
+++ src/utilities/AvoidNonclassUtils.java
@@ -2,12 +2,14 @@ package utilities;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.regex.Pattern;
 
 public class AvoidNonclassUtils {
 	
-	private static String[] nonclass = {"B","C","D","F","I","J","S","Z","V",">","*", "JI",")V",")I",")Z","IIIII",")F","ZZ","JJ","BB","SS","CC","FF","DD","TT",")J","ZJ","IZ",")TT"};
+	private static String[] nonclass = {"B","C","D","F","I","J","S","Z","V",">","*", "JI",")V",")I",")Z","IIIII",")F","ZZ","JJ","BB","SS","CC","FF","DD","TT",")J","ZJ","IZ",")TT",")TE",")C","BII","CII","II"};
 	private static List<String> classes = Arrays.asList(nonclass);
 	public static boolean isAClass(String signature){
+		//Pattern pattern = new Pattern("");
 		if(signature == null||signature.equals("")) return false;
 		int index = 0;
 		while(signature.charAt(index)== '['){
